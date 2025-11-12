62b4dec8c808803f5e65bf4084e7df5809e5c4be
Maksim Bengraf <me@octobanon.com>
2025-11-13 00:14:07 +0700

    feat(updater/platorm): add macOS x86_64 and arm64 target support

--- src/updater/platform_check.rs
+++ src/updater/platform_check.rs
@@ -2,6 +2,8 @@ pub fn target_suffix() -> &'static str {
     match () {
         _ if cfg!(all(target_os = "windows", target_arch = "x86_64")) => "windows-x86_64.exe",
         _ if cfg!(all(target_os = "linux", target_arch = "x86_64")) => "linux-x86_64",
+        _ if cfg!(all(target_os = "macos", target_arch = "x86_64")) => "macos-x86_64",
+        _ if cfg!(all(target_os = "macos", target_arch = "aarch64")) => "macos-arm64",
         _ => "unknown",
     }
 }
